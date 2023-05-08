509a717e7ab83bfcf44bb255199855620c988be5
João Bernardino <joao.bernardino@taqtile.com.br>
2023-05-08 17:19:31 -0300

    Rename hash function

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,7 +1,7 @@
 import { UserInput } from './schema';
 import { User } from './entity/user';
 import { validateUser } from './user-validation';
-import { hashPassword } from './hash-password';
+import { hashString } from './hash-string';
 
 export const resolvers = {
   Query: {
@@ -15,7 +15,7 @@ export const resolvers = {
       if (!validationResult.validated) {
         throw new Error(validationResult.failureReason);
       }
-      newUser.password = await hashPassword(newUser.password);
+      newUser.password = await hashString(newUser.password);
 
       await newUser.save();
       return newUser;
