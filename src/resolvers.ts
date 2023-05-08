55f0b06e85d51213c0eb63bd7fcb1ad4f46580f0
João Bernardino <joao.bernardino@taqtile.com.br>
2023-05-08 09:06:55 -0300

    Perform validation in resolver

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -7,11 +7,18 @@ export const resolvers = {
   },
   Mutation: {
     createUser: async (_: unknown, { data }: { data: UserInput }) => {
+      if (!User.isValidPassword(data.password)) {
+        throw new Error('Invalid password');
+      } else if (!(await User.isValidEmail(data.email))) {
+        throw new Error('Invalid email');
+      }
+
       const newUser = new User();
       newUser.name = data.name;
       newUser.email = data.email;
       newUser.password = data.password;
       newUser.birthDate = data.birthDate;
+
       await newUser.save();
       return newUser;
     },
