cc4554788190d11b7f1ea7298564feb66ab42ed4
João Bernardino <joao.bernardino@taqtile.com.br>
2023-05-10 14:18:33 -0300

    Detailed error messages for userValidation

--- src/user-validation.ts
+++ src/user-validation.ts
@@ -5,26 +5,47 @@ import { UserInput } from './schema';
 function validatePassword(password: string) {
   const digitRegex = /[0-9]/;
   const letterRegex = /[a-z|A-Z]/;
-  return password.length > 5 && digitRegex.test(password) && letterRegex.test(password);
+
+  const validations = [
+    { validated: password.length > 5, failureReason: 'password should have at least 6 characters' },
+    { validated: digitRegex.test(password), failureReason: 'password should contain a digit' },
+    { validated: letterRegex.test(password), failureReason: 'password should contain a letter' },
+  ];
+
+  const validationResult = validations.find((value) => value.validated == false);
+  if (validationResult) {
+    return { message: 'invalid password', ...validationResult };
+  } else {
+    return { validated: true, message: '', failureReason: '' };
+  }
 }
 
 async function validateEmail(email: string) {
   const userRepository = appDataSource.getRepository(User);
   const isDuplicated = await userRepository.exist({ where: { email } });
   const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
-  return !isDuplicated && emailRegex.test(email);
+
+  if (isDuplicated) {
+    return {
+      validated: false,
+      message: 'invalid email',
+      failureReason: 'an user with that email already exists',
+    };
+  } else if (!emailRegex.test(email)) {
+    return {
+      validated: false,
+      message: 'invalid email',
+      failureReason: 'email has invalid format',
+    };
+  } else {
+    return { validated: true, message: '', failureReason: '' };
+  }
 }
 
 export async function validateUser(userInput: UserInput) {
-  let validated = true;
-  let failureReason = '';
-
-  if (!validatePassword(userInput.password)) {
-    validated = false;
-    failureReason = 'Invalid Password';
-  } else if (!(await validateEmail(userInput.email))) {
-    validated = false;
-    failureReason = 'Invalid Email';
+  let validationResult = validatePassword(userInput.password);
+  if (validationResult.validated) {
+    validationResult = await validateEmail(userInput.email);
   }
-  return { validated, failureReason };
+  return validationResult;
 }
