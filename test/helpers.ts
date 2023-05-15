10016ed65912dd049269fa1f3bb1e0f165339d80
João Bernardino <joao.bernardino@taqtile.com.br>
2023-05-15 16:17:15 -0300

    Tests

--- test/helpers.ts
+++ test/helpers.ts
@@ -3,9 +3,18 @@ import { User } from '../src/entity/user';
 
 export const userRepository = appDataSource.getRepository(User);
 
+// mock user data
 export const defaultUserInput = {
   birthDate: '2003-19-01',
   email: 'john.smith@email.com',
   name: 'John Smith',
+  password: 'password123',
+};
+
+// tests that require authentication authenticate as the 'tester' user
+export const testerUserInput = {
+  birthDate: '2003-19-01',
+  email: 'tester@email.com',
+  name: 'tester',
   password: 'password123',
 };
