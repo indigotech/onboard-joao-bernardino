2bece5392cdd76732edb59f61cd773ec5983bbf4
João Bernardino <joao.bernardino@taqtile.com.br>
2023-05-15 12:13:29 -0300

    Reorganize tests

--- test/index.ts
+++ test/index.ts
@@ -3,20 +3,14 @@ import { use as chaiUse } from 'chai';
 import chaiExclude from 'chai-exclude';
 import { setupEnv } from '../src/environment';
 import { run } from '../src/server';
-import { testLoginMutation } from './login-mutation';
-import { testCreateUserMutation } from './create-user-mutation';
-
-chaiUse(chaiExclude);
-
-let serverUrl: string;
 
 before(async () => {
+  chaiUse(chaiExclude);
   setupEnv();
-  serverUrl = `http://localhost:${process.env.PORT}/`;
   await run();
 });
 
 describe('Mutation', () => {
-  testCreateUserMutation(() => serverUrl);
-  testLoginMutation(() => serverUrl);
+  require('./create-user-mutation');
+  require('./login-mutation');
 });
--- test/login-mutation/index.ts
+++ test/login-mutation/index.ts
@@ -1,74 +1,76 @@
 import { describe, it, beforeEach } from 'mocha';
-import { expect, use as chaiUse } from 'chai';
+import { expect } from 'chai';
 import * as jwt from 'jsonwebtoken';
 import chaiExclude from 'chai-exclude';
 import { requestLogin, defaultLoginInput, insertUserInDB } from './helpers';
 import { userRepository } from '../helpers';
 
-chaiUse(chaiExclude);
+describe('login', () => {
+  let serverUrl: string;
 
-export function testLoginMutation(getServerUrl: () => string) {
-  describe('login', () => {
-    beforeEach(async () => {
-      await userRepository.delete({});
+  before(() => {
+    serverUrl = `http://localhost:${process.env.PORT}/`;
+  });
+
+  beforeEach(async () => {
+    await userRepository.delete({});
+  });
+
+  it('should succeed if credentials are correct', async () => {
+    const newUser = await insertUserInDB(defaultLoginInput);
+
+    const response = (await requestLogin(defaultLoginInput, serverUrl)).data;
+    const loginInfo = response.data.login;
+
+    expect(loginInfo.user).to.deep.equal({
+      id: newUser.id.toString(),
+      name: newUser.name,
+      email: newUser.email,
+      birthDate: newUser.birthDate,
     });
 
-    it('should succeed if credentials are correct', async () => {
-      const newUser = await insertUserInDB(defaultLoginInput);
+    const jwtPayload = jwt.verify(loginInfo.token, process.env.JWT_PRIVATE_KEY!) as jwt.JwtPayload;
+    expect(jwtPayload).keys(['id', 'exp', 'iat']);
+    expect(jwtPayload.id).to.equal(newUser.id);
+    expect((jwtPayload.exp! - jwtPayload.iat!) / 3600).to.equal(+process.env.JWT_EXPIRATION_HOURS!);
+  });
 
-      const response = (await requestLogin(defaultLoginInput, getServerUrl())).data;
-      const loginInfo = response.data.login;
+  it('should extend token lifetime if remeberMe is true', async () => {
+    const newUser = await insertUserInDB(defaultLoginInput);
 
-      expect(loginInfo.user).to.deep.equal({
-        id: newUser.id.toString(),
-        name: newUser.name,
-        email: newUser.email,
-        birthDate: newUser.birthDate,
-      });
+    const response = (await requestLogin({ ...defaultLoginInput, rememberMe: true }, serverUrl)).data;
+    const loginInfo = response.data.login;
 
-      const jwtPayload = jwt.verify(loginInfo.token, process.env.JWT_PRIVATE_KEY!) as jwt.JwtPayload;
-      expect(jwtPayload).keys(['id', 'exp', 'iat']);
-      expect(jwtPayload.id).to.equal(newUser.id);
-      expect((jwtPayload.exp! - jwtPayload.iat!) / 3600).to.equal(+process.env.JWT_EXPIRATION_HOURS!);
-    });
+    expect(loginInfo.user).excluding(['password', 'id']).to.deep.equal(newUser);
+    expect(+loginInfo.user.id).to.deep.equal(newUser.id);
 
-    it('should extend token lifetime if remeberMe is true', async () => {
-      const newUser = await insertUserInDB(defaultLoginInput);
+    const jwtPayload = jwt.verify(loginInfo.token, process.env.JWT_PRIVATE_KEY!) as jwt.JwtPayload;
+    expect(jwtPayload).keys(['id', 'exp', 'iat']);
+    expect(jwtPayload.id).to.equal(newUser.id);
+    expect((jwtPayload.exp! - jwtPayload.iat!) / 3600).to.equal(+process.env.JWT_EXTENDED_EXPIRATION_HOURS!);
+  });
 
-      const response = (await requestLogin({ ...defaultLoginInput, rememberMe: true }, getServerUrl())).data;
-      const loginInfo = response.data.login;
+  it('should fail if no user has the given email', async () => {
+    await insertUserInDB(defaultLoginInput);
 
-      expect(loginInfo.user).excluding(['password', 'id']).to.deep.equal(newUser);
-      expect(+loginInfo.user.id).to.deep.equal(newUser.id);
+    const response = (await requestLogin({ ...defaultLoginInput, email: 'wrong@email.com' }, serverUrl)).data;
 
-      const jwtPayload = jwt.verify(loginInfo.token, process.env.JWT_PRIVATE_KEY!) as jwt.JwtPayload;
-      expect(jwtPayload).keys(['id', 'exp', 'iat']);
-      expect(jwtPayload.id).to.equal(newUser.id);
-      expect((jwtPayload.exp! - jwtPayload.iat!) / 3600).to.equal(+process.env.JWT_EXTENDED_EXPIRATION_HOURS!);
-    });
-
-    it('should fail if no user has the given email', async () => {
-      await insertUserInDB(defaultLoginInput);
-
-      const response = (await requestLogin({ ...defaultLoginInput, email: 'wrong@email.com' }, getServerUrl())).data;
-
-      expect(response.errors[0]).excluding('stacktrace').to.deep.equal({
-        message: 'Invalid credentials',
-        code: 401,
-        details: 'Wrong email or password',
-      });
-    });
-
-    it('should fail if password is wrong', async () => {
-      await insertUserInDB(defaultLoginInput);
-
-      const response = (await requestLogin({ ...defaultLoginInput, password: 'wr0ngpassword' }, getServerUrl())).data;
-
-      expect(response.errors[0]).excluding('stacktrace').to.deep.equal({
-        message: 'Invalid credentials',
-        code: 401,
-        details: 'Wrong email or password',
-      });
+    expect(response.errors[0]).excluding('stacktrace').to.deep.equal({
+      message: 'Invalid credentials',
+      code: 401,
+      details: 'Wrong email or password',
     });
   });
-}
+
+  it('should fail if password is wrong', async () => {
+    await insertUserInDB(defaultLoginInput);
+
+    const response = (await requestLogin({ ...defaultLoginInput, password: 'wr0ngpassword' }, serverUrl)).data;
+
+    expect(response.errors[0]).excluding('stacktrace').to.deep.equal({
+      message: 'Invalid credentials',
+      code: 401,
+      details: 'Wrong email or password',
+    });
+  });
+});
