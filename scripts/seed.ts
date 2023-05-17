ee16db3913b343b54d72ea0f8ce80cfc7ab9f53f
João Bernardino <joao.bernardino@taqtile.com.br>
2023-05-16 21:57:24 -0300

    Setup database seed script

---
+++ scripts/seed.ts
@@ -0,0 +1,35 @@
+import * as dotenv from 'dotenv';
+import { Faker, pt_BR } from '@faker-js/faker';
+import { hash } from 'bcrypt';
+import { appDataSource } from 'src/data-source';
+import { User } from 'src/entity/user';
+
+const faker = new Faker({ locale: [pt_BR] });
+
+async function makeFakeUser() {
+  const user = new User();
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+  const birthDate = faker.date.birthdate();
+
+  Object.assign(user, {
+    name: firstName + ' ' + lastName,
+    email: faker.internet.email({ firstName, lastName }),
+    birthDate: birthDate.getDay() + '/' + birthDate.getMonth() + '/' + birthDate.getFullYear(),
+    password: await hash(faker.internet.password({ length: 20 }), 10),
+  });
+
+  return user;
+}
+
+async function seed() {
+  dotenv.config({ path: 'test.env' });
+  await appDataSource.setOptions({ url: process.env.DB_URL }).initialize();
+  const userRepository = appDataSource.getRepository(User);
+
+  for (let i = 0; i <= 50; i++) {
+    userRepository.save(await makeFakeUser());
+  }
+}
+
+seed();
