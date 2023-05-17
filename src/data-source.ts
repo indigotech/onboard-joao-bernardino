bb63ad0097b34ae3a76d07324594ce5375b149a9
João Bernardino <joao.bernardino@taqtile.com.br>
2023-05-17 16:19:54 -0300

    Add Address entity

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,13 +1,14 @@
 import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { User } from 'src/entity/user';
+import { Address } from 'src/entity/address';
 
 export const appDataSource = new DataSource({
   type: 'postgres',
   url: '',
   synchronize: true,
   logging: false,
-  entities: [User],
+  entities: [User, Address],
   migrations: [],
   subscribers: [],
 });
