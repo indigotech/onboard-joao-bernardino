5d291658155aaede52018935f3918e05217a3a0e
João Bernardino <joao.bernardino@taqtile.com.br>
2023-05-18 10:57:24 -0300

    Fix linter errors

--- test/user-pagination.test.ts
+++ test/user-pagination.test.ts
@@ -6,56 +6,57 @@ import { usersQuery } from 'test/graphql-snippets';
 import { saveFakeUsers } from 'scripts/seed';
 
 describe('users query', () => {
-
   afterEach(async () => {
     await userRepository.delete({});
   });
 
   it('should only send the specified number of users', async () => {
-    let token = getValidToken();
+    const token = getValidToken();
     await saveFakeUsers(20);
 
-    let res = (await makeRequest({ query: usersQuery, variables: { count: 15, offset: 0}, token })).data;
+    const res = (await makeRequest({ query: usersQuery, variables: { count: 15, offset: 0 }, token })).data;
 
     expect(res.data.users.users.length).to.be.equal(15);
   });
 
   it('should set hasNextPage and hasPreviousPage correctly when in the last page', async () => {
-    let token = getValidToken();
+    const token = getValidToken();
     await saveFakeUsers(15);
 
-    let res = (await makeRequest({ query: usersQuery, variables: { count: 10, offset: 10}, token })).data;
+    const res = (await makeRequest({ query: usersQuery, variables: { count: 10, offset: 10 }, token })).data;
 
     expect(res.data.users.hasNextPage).to.be.false;
     expect(res.data.users.hasPreviousPage).to.be.true;
   });
 
   it('should set hasNextPage and hasPreviousPage correctly when in the first page', async () => {
-    let token = getValidToken();
+    const token = getValidToken();
     await saveFakeUsers(15);
 
-    let res = (await makeRequest({ query: usersQuery, variables: { count: 10, offset: 0}, token })).data;
+    const res = (await makeRequest({ query: usersQuery, variables: { count: 10, offset: 0 }, token })).data;
 
     expect(res.data.users.hasNextPage).to.be.true;
     expect(res.data.users.hasPreviousPage).to.be.false;
   });
 
   it('should return the correct total number of users in the database', async () => {
-    let token = getValidToken();
+    const token = getValidToken();
     await saveFakeUsers(20);
 
-    let res = (await makeRequest({ query: usersQuery, variables: { count: 10, offset: 0}, token })).data;
+    const res = (await makeRequest({ query: usersQuery, variables: { count: 10, offset: 0 }, token })).data;
 
     expect(res.data.users.totalNumberOfUsers).to.be.equal(20);
   });
 
   it('should return the users sorted by name', async () => {
-    let token = getValidToken();
+    const token = getValidToken();
     await saveFakeUsers(20);
 
-    let res = (await makeRequest({ query: usersQuery, variables: { count: 10, offset: 0}, token })).data;
+    const res = (await makeRequest({ query: usersQuery, variables: { count: 10, offset: 0 }, token })).data;
 
-    const names = res.data.users.users.map((u: {id:string; name:string; birthDate:string; email:string}) => u.name);
+    const names = res.data.users.users.map(
+      (u: { id: string; name: string; birthDate: string; email: string }) => u.name,
+    );
     expect(names).to.be.deep.equal([...names].sort());
   });
 });
